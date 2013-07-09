fa9adda91928c8fdef134221a9d7de400aa4a40c
Long On <on.long.on@goomail.fake>
2013-07-09 01:10:24 -0400

    Add modal popup function

---
+++ src/jquery-ui-apptk.js
@@ -0,0 +1,53 @@
+
+(function($) {
+  $.fn.apptk = function(options) {
+
+    var slef = $( this );
+
+    //
+    // create a modal popup on caller dom, with defaults
+    //   title:  - popup title
+    //   height: - default height is 240px
+    //   width:  - default width is 500px
+    //   show:   - default show effect is puff,
+    //      see http://api.jqueryui.com/category/effects/
+    //   closebtn_text: - default button text is 'Close'
+    //
+    $.popup = function(opts) {
+      var params = $.extend({
+        title:  "Title",
+        height: 240,
+        width:  500,
+        show:   "puff",
+        closebtn_text: "Close"
+      }, opts);
+
+      slef.dialog({
+        title: params.title,
+        show: params.show,
+        height: params.height,
+        width:params.width,
+        autoOpen: false,
+        modal: true,
+        buttons: [{
+          text: params.closebtn_text,
+          click: function() {
+            $( this ).dialog("close");
+          }
+        }]
+      });
+    };
+
+    //
+    // options.method === "popup"
+    //
+    if (options.method === "popup") {
+       $.popup(options);
+      return this;
+    };
+
+
+  }
+}
+)(jQuery);
+
